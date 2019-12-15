7fe08bb376ca50b596d92174149635c36d9d6c4b
ahlemchiboub <57589922+ahlemchiboub@users.noreply.github.com>
2019-12-15 13:01:32 +0100

    Add files via upload

---
+++ ahlem/js/main.js
@@ -0,0 +1,21 @@
+	var myString="hello"; 
+	var secondString="world"; 
+	alert(myString.toLowerCase()); 
+	alert(secondString.toUpperCase()); 
+
+	var myString="Hello There,This is a split test"; 
+	var parts=myString.split(","); 
+	alert(parts); 
+
+
+ 
+ var getId=document.getElementById("myPara").innerHTML; 
+
+ var getClass=document.getElementByClassName("Tracks"); 
+
+ var getParagraph=document.getElementByClassName("p"); 
+
+ console.log(getId); 
+ console.log(getClass); 
+ console.log(getParagraph);
+
